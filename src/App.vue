b9929d1957dbbc9b929c214bb649803ef06e4724
danilPorkchop15 <danilkostin150705@gmail.com>
2024-01-13 23:44:15 +0300

    asdas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <heart />
+  <div id="lisa"/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import heart from "./components/heart.vue";
 </script>
 
 <style>
+html {
+  background: #ffb7e3;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  place-items: center;
+  background: rgb(255, 236, 252);
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+.abs {
+  position: absolute;
+  z-index: 999;
+}
+.heart {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-block: 2rem;
+  transition: all ease-in-out 0.5s;
+}
+.heart > * {
+  transition: all ease-in-out 0.5s;
+}
+.clicked > #cat_left {
+  margin-right: 32vw;
+}
+.clicked {
+  scale: 1.2;
+}
+.finger {
+  height: 10vh;
+  animation: fingerMove 1s infinite;
+}
+@media screen and (max-width: 700px) {
+  #cat_left,
+  #cat_right {
+    height: 20vh;
+  }
+  .finger {
+    height: 5vh;
+    transition: all ease 1s;
+  }
+}
+
+@keyframes fingerMove {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(20%);
+  }
+}
+.fingers_1,
+.fingers_2 {
+  display: flex;
+  justify-content: space-between;
+  gap: 3rem;
+}
+.fingers_1 {
+  rotate: 180deg;
+}
+#lisa {
+  height: 10vh;
+  background-image: url("./assets/lisa.png");
+  background-size: cover;
+  width: 60px;
+  transition: all ease-out 1s;
 }
 </style>
---
+++ src/components/heart.vue
@@ -0,0 +1,80 @@
+<script setup>
+const useConfetti = () => {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0.2,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+
+  confetti({
+    ...defaults,
+    particleCount: 70,
+    scalar: 2,
+  });
+
+confetti({
+  ...defaults,
+  particleCount: 50,
+  scalar: 3,
+});
+
+confetti({
+  ...defaults,
+  particleCount: 25,
+  scalar: 4,
+});
+
+confetti({
+  ...defaults,
+  particleCount: 10,
+  scalar: 5,
+});
+};
+
+const expandHeart = () => {
+  useConfetti();
+  document.getElementById("heart").classList.add("clicked");
+  document.getElementById("lisa").style.scale = "3.3";
+  document.getElementById("lisa").style.zIndex = "1000";
+  let elements = document.querySelectorAll('.finger');
+
+// Применяем свойства ко всем найденным элементам
+elements.forEach(function(element) {
+    element.style.marginTop = "1000px ";  // Пример добавления свойства color
+    // Добавьте здесь другие свойства по необходимости
+});
+};
+</script>
+
+<template>
+  <div class="abs">
+    <div class="fingers_1">
+      <img
+        src="../assets/finger.png"
+        alt=""
+        class="finger"
+        v-for="index in 3"
+        :key="index"
+      />
+    </div>
+    <div class="heart" @click="expandHeart" id="heart">
+      <img src="../assets/1.png" alt="" id="cat_left" />
+      <img src="../assets/2.png" alt="" id="cat_right" />
+    </div>
+    <div class="fingers_2">
+      <img
+        src="../assets/finger.png"
+        alt=""
+        class="finger"
+        v-for="index in 3"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+
+<style></style>
